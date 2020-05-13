7403072e9b371abbc1603ebfb8c19328ca473c21
Onno <o.m.gieling@student.tudelft.nl>
2020-05-13 17:03:10 +0200

    Modify example to match the cppexample

--- src/gdexample.cpp
+++ src/gdexample.cpp
@@ -1,20 +1,20 @@
 #include "gdexample.h"
-#include <opencv2/core/core.hpp>
+
+#include <opencv2/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
+using namespace godot;
 using namespace cv;
 using namespace std;
-using namespace godot;
 
 void GDExample::_register_methods() {
     register_method("_process", &GDExample::_process);
     register_property<GDExample, float>("amplitude", &GDExample::amplitude, 10.0);
+    register_property<GDExample, float>("speed", &GDExample::set_speed, &GDExample::get_speed, 1.0);
 
-    // First is name of signal
-    // After that you have pairs of values that specify parameter name and type of each parameter we send to signal
-    register_signal<GDExample>((char*)"position_changed", "node", GODOT_VARIANT_TYPE_OBJECT, "new_pos", GODOT_VARIANT_TYPE_VECTOR2);
+    register_signal<GDExample>((char *)"position_changed", "node", GODOT_VARIANT_TYPE_OBJECT, "new_pos", GODOT_VARIANT_TYPE_VECTOR2);
 }
 
 GDExample::GDExample() {
@@ -27,17 +27,18 @@ GDExample::~GDExample() {
 void GDExample::_init() {
     // initialize any variables here
     time_passed = 0.0;
-    time_emit = 0.0;
     amplitude = 10.0;
-
+    speed = 1.0;
     Mat image = Mat::zeros(300, 600, CV_8UC3);
     circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
-    // circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
-    // imshow("Display Window", image);
+    circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
+    imshow("Display Window", image);
+    waitKey(0);
+
 }
 
 void GDExample::_process(float delta) {
-    time_passed += delta;
+    time_passed += speed * delta;
 
     Vector2 new_position = Vector2(
         amplitude + (amplitude * sin(time_passed * 2.0)),
@@ -47,9 +48,17 @@ void GDExample::_process(float delta) {
     set_position(new_position);
 
     time_emit += delta;
-    // If a second has passed
     if (time_emit > 1.0) {
         emit_signal("position_changed", this, new_position);
+
         time_emit = 0.0;
     }
+}
+
+void GDExample::set_speed(float p_speed) {
+    speed = p_speed;
+}
+
+float GDExample::get_speed() {
+    return speed;
 }
