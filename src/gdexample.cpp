59ebd9c12f161b99d366f2c12f688194f8193567
Kevin van der Werff <k.s.a.vanderwerff@student.tudelft.nl>
2020-05-25 14:34:03 +0200

    declare dependencies

--- src/gdexample.cpp
+++ src/gdexample.cpp
@@ -1,7 +1,7 @@
 #include "gdexample.h"
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
 using namespace cv;
@@ -32,8 +32,8 @@ void GDExample::_init() {
 
     Mat image = Mat::zeros(300, 600, CV_8UC3);
     circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
-    // circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
-    // imshow("Display Window", image);
+    circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
+    imshow("Display Window", image);
 }
 
 void GDExample::_process(float delta) {
